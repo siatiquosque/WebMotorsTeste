fa40cf9ba7996b6bf62df6a40b97f9193c862880
Felipe Siatiquosque de Lima <felipe.slima@grupopan.com>
2019-08-26 12:05:57 -0300

    Add Splash

---
+++ app/src/main/java/br/com/siatiquosque/webmotorstest/SplashScreen.kt
@@ -0,0 +1,19 @@
+package br.com.siatiquosque.webmotorstest
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
